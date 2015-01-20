5adc660379b116d012a01be8704187fb28aa29b8
GrahamTheCoder <GrahamTheCoder@gmail.com>
2015-01-20 16:30:51 +0000

    Commit failing test

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public class StandardSnapValidatorTests
+    {
+        [Test]
+        public void EmptyStackIsNotSnap()
+        {
+            var stack = new Stack(Enumerable.Empty<Card>());
+            var isSnap = IsSnap(stack);
+            Assert.That(isSnap, Is.False);
+        }
+
+        private bool IsSnap(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

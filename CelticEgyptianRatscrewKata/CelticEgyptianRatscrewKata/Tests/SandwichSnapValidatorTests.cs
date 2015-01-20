cd91924faeec6f8bdd3c3816ff554a87a8f31401
GrahamTheCoder <GrahamTheCoder@gmail.com>
2015-01-20 17:01:32 +0000

    New failing test

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapValidatorTests.cs
@@ -19,6 +19,17 @@ namespace CelticEgyptianRatscrewKata.Tests
             Assert.That(isSnap, Is.False);
         }
 
+        [Test]
+        public void ASandwichIsASnap()
+        {
+            var card1 = new Card(Suit.Clubs, Rank.Two);
+            var card2 = new Card(Suit.Clubs, Rank.Ace);
+
+            var stack = CreateStack(card1, card2, card1);
+            var isSnap = m_SnapValidator.IsSnap(stack);
+            Assert.That(isSnap, Is.True);
+        }
+
         private static Stack CreateStack(params Card[] cards)
         {
             return new Stack(cards);

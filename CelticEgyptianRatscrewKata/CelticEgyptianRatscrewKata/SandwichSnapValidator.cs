4d8ad22dc868eb2414e87cea1c9d10f80d4d0cdc
GrahamTheCoder <GrahamTheCoder@gmail.com>
2015-01-20 16:52:34 +0000

    Add failing test for sandwich case

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnapValidator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CelticEgyptianRatscrewKata
+{
+    internal class SandwichSnapValidator : ISnapValidator
+    {
+        public bool IsSnap(Stack stack)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    [TestFixture]
+    public class SandwichSnapValidatorTests
+    {
+        private readonly ISnapValidator m_SnapValidator = new SandwichSnapValidator();
+
+        [Test]
+        public void EmptyStackIsNotSnap()
+        {
+            var stack = CreateStack();
+            var isSnap = m_SnapValidator.IsSnap(stack);
+            Assert.That(isSnap, Is.False);
+        }
+
+        private static Stack CreateStack(params Card[] cards)
+        {
+            return new Stack(cards);
+        }
+    }
+}

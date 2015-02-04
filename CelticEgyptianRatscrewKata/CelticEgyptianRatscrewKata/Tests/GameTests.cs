f581266f2410cc97334920538a8c6eee86304fec
Graham Helliwell <graham.helliwell@red-gate.com>
2015-02-04 17:02:32 +0000

    defer winstate checking, give players some cards

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 
 namespace CelticEgyptianRatscrewKata.Tests
@@ -11,65 +12,85 @@ namespace CelticEgyptianRatscrewKata.Tests
     public class GameTests
     {
         [Test]
-        public void NoOneHasWonAtStartOfTwoPlayerGame()
-        {
-            var gameBuilder = new GameBuilder();
-            gameBuilder.AddPlayer(new Player("alice"));
-            gameBuilder.AddPlayer(new Player("bob"));
-            var winner = gameBuilder.Build().Winner;
-            Assert.That(winner, Is.Null);
-        }
-
-        [Test]
-        public void PlayerOneWinsInOnePlayerGame()
+        public void GameHasNoWinnerIfWinStateCheckerReturnsNull()
         {
             var gameBuilder = new GameBuilder();
             var alice = new Player("alice");
             gameBuilder.AddPlayer(alice);
-            var winner = gameBuilder.Build().Winner;
+            var winChecker = new Mock<IWinStateChecker>();
+            winChecker.Setup(x => x.GetWinningPlayer(It.IsAny<Game>())).Returns((Player?) null);
+            var game = gameBuilder.Build(winChecker.Object, Cards.Empty());
+            var winner = game.Winner;
+            Assert.That(winner, Is.EqualTo(null));
+        }
+
+        [Test]
+        public void GameHasWinnerIfWinStateCheckerReturnsWinner()
+        {
+            var gameBuilder = new GameBuilder();
+            var alice = new Player("alice");
+            gameBuilder.AddPlayer(alice);
+            var winChecker = new Mock<IWinStateChecker>();
+            winChecker.Setup(x => x.GetWinningPlayer(It.IsAny<Game>())).Returns(alice);
+            var game = gameBuilder.Build(winChecker.Object, Cards.Empty());
+            var winner = game.Winner;
             Assert.That(winner, Is.EqualTo(alice));
         }
+
+        [Test]
+        public void PlayerCannotPlayCardsIfTheyHaveNone()
+        {
+            var gameBuilder = new GameBuilder();
+            Player alice = new Player("alice");
+            gameBuilder.AddPlayer(alice);
+            var game = gameBuilder.Build(new WinStateChecker(), Cards.With(new Card(Suit.Spades, Rank.Ace)));
+
+            game.PlayCardFrom(alice);
+            Assert.That(() => game.PlayCardFrom(alice), Throws.InstanceOf<Exception>());
+
+        }
+    }
+
+    public interface IWinStateChecker
+    {
+        /// <returns>winning player or null</returns>
+        Player? GetWinningPlayer(Game game);
     }
 
 
     [TestFixture]
     public class GameBuilderTests
     {
-        [Test]
-        public void CanInitialize()
-        {
-            Assert.DoesNotThrow(() => new GameBuilder());
-        }
-
-        [Test]
-        public void CanAddPlayer()
-        {
-            var gameBuilder = new GameBuilder();
-            Assert.DoesNotThrow(() =>
-            {
-                gameBuilder.AddPlayer(new Player("bob"));
-            });
-        }
-
-        [Test]
+       [Test]
         public void CanBuildGame()
         {
             var gameBuilder = new GameBuilder();
             var player = new Player("bob");
             gameBuilder.AddPlayer(player);
-            var game = gameBuilder.Build();
+            var game = gameBuilder.Build(new WinStateChecker(), Cards.Empty());
             Assert.AreEqual(player, game.CurrentPlayer);
         }
     }
 
-    public class Player
+    public class WinStateChecker : IWinStateChecker
     {
-        public readonly string Bob;
-
-        public Player(string bob)
+        public Player? GetWinningPlayer(Game game)
         {
-            Bob = bob;
+            return null;
         }
+    }
+
+    public struct Player
+    {
+        public readonly string Name;
+
+        public Player(string name) : this()
+        {
+            Cards = Cards.Empty();
+            Name = name;
+        }
+
+        public Cards Cards { get; private set; }
     }
 
     public class GameBuilder
@@ -81,18 +102,25 @@ namespace CelticEgyptianRatscrewKata.Tests
             m_Players.Add(player);
         }
 
-        public Game Build()
+        public Game Build(IWinStateChecker winStateChecker, Cards cards)
         {
-            return new Game(m_Players);
+            if (cards.Any())
+            {
+                m_Players.First().Cards.AddToTop(cards.First());
+            }
+
+            return new Game(winStateChecker, m_Players);
         }
     }
 
     public class Game
     {
+        private readonly IWinStateChecker m_WinStateChecker;
         private readonly IReadOnlyCollection<Player> m_Players;
 
-        public Game(IReadOnlyCollection<Player> players)
+        public Game(IWinStateChecker winStateChecker, IReadOnlyCollection<Player> players)
         {
+            m_WinStateChecker = winStateChecker;
             m_Players = players;
         }
 
@@ -101,6 +129,11 @@ namespace CelticEgyptianRatscrewKata.Tests
             get { return m_Players.First(); }
         }
 
-        public Player Winner { get; private set; }
+        public Player? Winner { get { return m_WinStateChecker.GetWinningPlayer(this); } }
+
+        public void PlayCardFrom(Player player)
+        {
+            player.Cards.RemoveCardAt(0);
+        }
     }
 }

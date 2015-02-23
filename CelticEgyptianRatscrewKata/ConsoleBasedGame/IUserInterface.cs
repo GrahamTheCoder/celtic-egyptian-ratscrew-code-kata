85e4d00b8e7b576edcbe1cfa3b1ec46df4791a93
Graham Helliwell <graham.helliwell@red-gate.com>
2015-02-23 16:15:47 +0000

    R# extract interface

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/IUserInterface.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/IUserInterface.cs
@@ -1,6 +1,10 @@
-﻿namespace ConsoleBasedGame
+﻿using System.Collections.Generic;
+
+namespace ConsoleBasedGame
 {
     internal interface IUserInterface
     {
+        IEnumerable<PlayerInfo> GetPlayerInfoFromUserLazily();
+        bool TryReadUserInput(out char userInput);
     }
 }

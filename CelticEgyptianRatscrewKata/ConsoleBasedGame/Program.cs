e0ef9c0d8b76b1d7c38ef2cb1d9f47f2ac8918df
Graham Helliwell <graham.helliwell@red-gate.com>
2015-02-23 16:58:27 +0000

    Split out RatScrewGame

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
@@ -8,31 +8,9 @@ namespace ConsoleBasedGame
         static void Main(string[] args)
         {
             var userInterface = new ConsoleInterface();
-            var gameFactory = SetupGame(userInterface);
-            PlayGame(gameFactory, userInterface);
-        }
-
-        private static void PlayGame(GameController game, IGamePlayUserInterface userInterface)
-        {
-            game.StartGame(GameFactory.CreateFullDeckOfCards());
-
-            char userInput;
-            while (userInterface.TryReadUserInput(out userInput))
-            {
-            }
-        }
-
-        private static GameController SetupGame(IGameSetupUserInterface userInterface)
-        {
-            var gameFactory = new GameFactory();
-
-            IEnumerable<PlayerInfo> playerInfos = userInterface.GetPlayerInfoFromUserLazily();
-
-            foreach (PlayerInfo playerInfo in playerInfos)
-            {
-                gameFactory.AddPlayer(new Player(playerInfo.PlayerName));
-            }
-            return gameFactory.Create();
+            var ratscrewGame = new RatScrewGame();
+            var gameController = ratscrewGame.SetupGame(userInterface);
+            ratscrewGame.PlayGame(gameController, userInterface);
         }
     }
 }
---
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/RatScrewGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.Game;
+
+namespace ConsoleBasedGame
+{
+    class RatScrewGame
+    {
+        public void PlayGame(GameController game, IGamePlayUserInterface userInterface)
+        {
+            game.StartGame(GameFactory.CreateFullDeckOfCards());
+
+            char userInput;
+            while (userInterface.TryReadUserInput(out userInput))
+            {
+            }
+        }
+
+        public GameController SetupGame(IGameSetupUserInterface userInterface)
+        {
+            var gameFactory = new GameFactory();
+
+            IEnumerable<PlayerInfo> playerInfos = userInterface.GetPlayerInfoFromUserLazily();
+
+            foreach (PlayerInfo playerInfo in playerInfos)
+            {
+                gameFactory.AddPlayer(new Player(playerInfo.PlayerName));
+            }
+            return gameFactory.Create();
+        }
+    }
+}
